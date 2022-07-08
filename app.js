ed1dd4e5c9b5f85d4213f96ef0261bf488b99d34
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:39:53 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//alert ("Mi archivo está bien linkeado")
+
+const numero = prompt ('Ingresa el número al que quiera calcularle el porcentaje');
+const porcentaje = prompt ('Ingresa el valor del porcentaje a calcular')
+
+const resultado = (numero * porcentaje ) /100;
+
+alert ('El' + porcentaje + '% de ' + numero + 'es: ' + resultado + '.');
+
+console.log (alert);
